b02bca2f6adad1d83bd200ad44b5c63955a4f6f7
ashucornelius020-droid <ashucornelius020@gmail.com>
2025-08-21 17:06:01 +0100

    Update script.js

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,62 +1,16 @@
 const controls = document.querySelectorAll('.btn-input');
 const screenContent = document.querySelector('.screen input');
-const actions = document.querySelectorAll('.btn-action');
-const clear = document.querySelector('.btn-action-clear');
-const backspace = document.querySelector('.btn-input-backspace');
-console.log(actions);
+//const actions = document.querySelectorAll('.btn-action');
+//const clear = document.querySelector('.btn-action-clear');
+//const backspace = document.querySelector('.btn-input-backspace');
 
-const storage = {
+for(let index = 0; index < controls.length; index = index + 1){
+    const btn = controls[index];
 
-    NumberPressed: false,
-    OperatorPressed: false,
-    InMemory: "",
-};
+    btn.addEventListener('click', function(){ 
 
-for (let i = 0; i < controls.length; i++) {
-    const btn = controls[i];
-
-    btn.addEventListener('click', function () {
-        screenContent.value = screenContent.value + btn.textContent;
-        storage.NumberPressed = true;
-        storage.InMemory += btn.textContent;
-    });
-
-
+     //console.log(btn.textContent);
+     screenContent.value = screenContent.value + btn.textContent.trim;
+    } );
 }
 
-clear.addEventListener('click', function (event) {
-    screenContent.value = "";
-});
-
-backspace.addEventListener('click', function (event) {
-    const Content = screenContent.value;
-    const NewScreenContent = Content.slice(0, -1);
-    screenContent.value = NewScreenContent;
-});
-
-
-
-for (let i = 0; i < actions.length; i++) {
-    const btnAction = actions[i];
-
-    btnAction.addEventListener('click', function (event) {
-
-        if (btnAction.classList.contains('bracket')) {
-            const Content = Number(screenContent.value);
-            screenContent.value = "(" + Content + ")";
-
-        }
-
-        else if (btnAction.classList.contains('percentage')) {
-            const Content = Number(screenContent.value) / 100;
-            screenContent.value = Content;
-        }
-
-        else if (btnAction.classList.contains('plus')) {
-            const Content = Number(screenContent.value);
-            screenContent.append['+'];
-        }
-    });
-
-
-}
